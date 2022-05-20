22dc393e20b3967675a1e9aa9bf2f87e08014bd7
root <root@ip-172-31-0-91.ec2.internal>
2022-05-20 19:08:12 +0000

    Pipeline project

---
+++ src/main/java/com/example999/www/App.java
@@ -0,0 +1,13 @@
+package com.example999.www;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World-Andorid and Build Pipeline!" );
+    }
+}
